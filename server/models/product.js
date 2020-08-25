fc64f228f2444fb63ba8a908241550a8bc91ac40
Christian Zigah <efochristian@outlook.com>
2020-08-25 14:14:02 +0000

    Create Product Model

---
+++ server/models/product.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    category: {
+      type: Schema.Types.ObjectID,
+      ref: "Category"
+    },
+    owner: {
+      type: Schema.Types.ObjectID,
+      ref: "Owner"
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    productImage: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    stockQuantidy: {
+      type: Number,
+      required: true
+    },
+    rating: { type: Number }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = { Product };
