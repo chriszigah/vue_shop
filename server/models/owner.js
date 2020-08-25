8eff6ba74b50898a8bbdf77a46942ba827b20a59
Christian Zigah <efochristian@outlook.com>
2020-08-25 14:11:54 +0000

    Create Owner Model

---
+++ server/models/owner.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const ownerSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    about: {
+      type: String,
+      required: true
+    },
+    ownerImage: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Owner = mongoose.model("Owner", ownerSchema);
+
+module.exports = { Owner };
